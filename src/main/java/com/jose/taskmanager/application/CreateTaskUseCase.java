4b4f45e343f49e3e29dc7ab84854ef12bb8d7a36
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-07-04 18:34:16 -0300

    chore(spring): registrar CreateTaskUseCase como bean

---
+++ src/main/java/com/jose/taskmanager/application/CreateTaskUseCase.java
@@ -0,0 +1,24 @@
+package com.jose.taskmanager.application;
+
+import com.jose.taskmanager.application.input.CreateTaskInput;
+import com.jose.taskmanager.application.output.TaskOutput;
+import com.jose.taskmanager.domain.Task;
+import com.jose.taskmanager.domain.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateTaskUseCase {
+
+    private final TaskRepository repository;
+
+    public CreateTaskUseCase(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+
+    TaskOutput execute(CreateTaskInput input) {
+        var task = new Task(input.title(), input.description());
+        var saved = repository.save(task);
+        return TaskOutput.from(task);
+    }
+}
