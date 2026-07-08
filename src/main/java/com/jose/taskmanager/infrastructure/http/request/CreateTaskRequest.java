1c8d54d38763718c40ef2db8947005b7df051ebc
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-07-07 22:27:13 -0300

    fix(infrastructure): adicionar validações

--- src/main/java/com/jose/taskmanager/infrastructure/http/request/CreateTaskRequest.java
+++ src/main/java/com/jose/taskmanager/infrastructure/http/request/CreateTaskRequest.java
@@ -1,12 +1,20 @@
 package com.jose.taskmanager.infrastructure.http.request;
 
 import com.jose.taskmanager.application.input.CreateTaskInput;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.Optional;
 
-public record CreateTaskRequest(String title, Optional<String> description) {
+public record CreateTaskRequest(
+        @NotBlank
+        @Size(min = 3, max = 100)
+        String title,
+        Optional<@Size(max = 500)String> description) {
+
 
     public CreateTaskInput toInput() {
+
         return new CreateTaskInput(title, description);
     }
 }
